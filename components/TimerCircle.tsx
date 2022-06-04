6af9b8c8db03eb766f27f89b55af6744dafac253
JethroSama <kel73896@gmail.com>
2022-06-04 22:53:53 +0800

    add pause icon

--- components/TimerCircle.tsx
+++ components/TimerCircle.tsx
@@ -1,5 +1,6 @@
 import { TIMER_STATUS } from "../types";
-
+import PauseIcon from '../icons/pause.svg'
+import Image from "next/image";
 const cleanPercentage = (percentage: number) => {
     const isNegativeOrNaN = !Number.isFinite(+percentage) || percentage < 0; // we can set non-numbers to 0 here
     const isTooHigh = percentage > 100;
@@ -36,7 +37,7 @@ const Pie = ({ percentage, color, handleStart, handlePause, timerStatus }: PiePr
     const handleClick = timerStatus === TIMER_STATUS.RUNNING ? handlePause : handleStart;
 
     return (
-        <div onClick={handleClick} className="relative w-fit h-fit">
+        <div onClick={handleClick} className="relative w-fit h-fit cursor-pointer">
 
             <svg height={300} width={300}>
                 <g transform={`rotate(-90 ${"100 100"})`}>
@@ -45,9 +46,9 @@ const Pie = ({ percentage, color, handleStart, handlePause, timerStatus }: PiePr
                 </g>
             </svg>
             <div className="absolute top-0 left-0 flex flex-col items-center justify-center h-full w-full">
-                {timerStatus === TIMER_STATUS.PAUSED && (
-                    <div className="text-center text-white text-2xl">Paused</div>
-                )}
+                <div className={`opacity-0 ${timerStatus === TIMER_STATUS.PAUSED && 'opacity-100'} transition-all duration-500 ease-in-out text-center`}>
+                    <Image src={PauseIcon} objectFit="fill" className="invert" />
+                </div>
             </div>
         </div >
     );
