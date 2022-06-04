e708899b3fb42a3639d675bb4607898e77a1d17e
JethroSama <kel73896@gmail.com>
2022-06-04 19:05:36 +0800

    Format timer func

--- lib/timer.ts
+++ lib/timer.ts
@@ -2,7 +2,7 @@ const getFormattedTime = (timestamp: number) => {
     const hours = Math.floor(timestamp / 60 / 60)
     const minutes = Math.floor(timestamp / 60) - (hours * 60)
     const seconds = timestamp % 60
-    return `${hours}:${minutes}:${seconds}`
+    return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
 
 }
 export default getFormattedTime
