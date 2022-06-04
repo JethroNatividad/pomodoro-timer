d27870c5bc79ebfb8b23b55aa469ce17ee273f11
JethroSama <kel73896@gmail.com>
2022-06-04 19:01:39 +0800

    implement getFormattedTime func

---
+++ lib/timer.ts
@@ -0,0 +1,8 @@
+const getFormattedTime = (timestamp: number) => {
+    const hours = Math.floor(timestamp / 60 / 60)
+    const minutes = Math.floor(timestamp / 60) - (hours * 60)
+    const seconds = timestamp % 60
+    return `${hours}:${minutes}:${seconds}`
+
+}
+export default getFormattedTime
