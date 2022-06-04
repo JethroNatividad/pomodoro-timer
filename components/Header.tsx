d705c0dd217f53441bac34ffa4db474a83a7e576
JethroSama <kel73896@gmail.com>
2022-06-04 18:52:07 +0800

    Style Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
-
+import TomatoIcon from '../icons/tomato.svg'
 type Props = {
     workDone: number
 }
 
 const Header = ({ workDone }: Props) => {
     return (
-        <div>
-            <h1>Pomodoro</h1>
+        <div className='shadow-lg h-16 flex items-center px-2'>
+            <div className='h-16 w-16 flex items-center relative'>
+                <Image src={TomatoIcon} objectFit="fill" />
+                <div className='absolute top-1 left-0 w-full h-full flex items-center justify-center'>
+                    <p className='text-lg font-semibold text-white'>{workDone}</p>
+                </div>
+            </div>
+            <h1 className='text-2xl lg:text-3xl font-semibold text-white drop-shadow-sm'>Pomodoro</h1>
         </div>
     )
 }
