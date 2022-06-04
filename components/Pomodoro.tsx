71166bb429217c9c5de2ff59f632567fa20a488c
JethroSama <kel73896@gmail.com>
2022-06-04 12:49:13 +0800

    Create test timer

--- components/Pomodoro.tsx
+++ components/Pomodoro.tsx
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 type Props = {}
 
 const Pomodoro = (props: Props) => {
+    const Ref = useRef<NodeJS.Timer | null>(null);
+    const [timer, setTimer] = useState<string>('00:00:00');
+
+    const getTimeRemaining = (e: string) => {
+        const total = Date.parse(e) - Date.parse(new Date().toString());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+        return {
+            total, hours, minutes, seconds
+        };
+    }
+
+    const startTimer = (e: string) => {
+        let { total, hours, minutes, seconds }
+            = getTimeRemaining(e);
+        if (total >= 0) {
+
+            // update the timer
+            // check if less than 10 then we need to 
+            // add '0' at the begining of the variable
+            setTimer(
+                (hours > 9 ? hours : '0' + hours) + ':' +
+                (minutes > 9 ? minutes : '0' + minutes) + ':'
+                + (seconds > 9 ? seconds : '0' + seconds)
+            )
+        }
+    }
+
+    const clearTimer = (e: string) => {
+
+        // If you adjust it you should also need to
+        // adjust the Endtime formula we are about
+        // to code next    
+        setTimer('00:00:10');
+
+        // If you try to remove this line the 
+        // updating of timer Variable will be
+        // after 1000ms or 1sec
+        if (Ref.current) clearInterval(Ref.current);
+        const id = setInterval(() => {
+            startTimer(e);
+        }, 1000)
+        Ref.current = id;
+    }
+
+    const getDeadTime = () => {
+        let deadline = new Date();
+
+        // This is where you need to adjust if 
+        // you entend to add more time
+        deadline.setSeconds(deadline.getSeconds() + 10);
+        return deadline;
+    }
+
+    useEffect(() => {
+        clearTimer(getDeadTime().toString());
+    }, []);
+
+    const onClickReset = () => {
+        clearTimer(getDeadTime().toString());
+    }
+
     return (
-        <div>Pomodoro</div>
+        <div>
+            <h2>{timer}</h2>
+            <button onClick={onClickReset}>Reset</button>
+        </div>
     )
 }
 
