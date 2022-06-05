1afabb8e77603b35eaef8930b6517cbb807e4ba4
JethroSama <kel73896@gmail.com>
2022-06-05 13:46:11 +0800

    Add Arrows, style arrows

--- components/PomodoroTimer.tsx
+++ components/PomodoroTimer.tsx
@@ -3,6 +3,8 @@ import getFormattedTime, { calculatePercentage, getStatusText, nextStatus, previ
 import { AppSettings, STATUS, TIMER_STATUS } from '../types'
 import Pie from './TimerCircle'
 import ResetIcon from '../icons/reset.svg'
+import PreviousIcon from '../icons/previous.svg'
+import NextIcon from '../icons/next.svg'
 import Image from 'next/image'
 
 type Props = {
@@ -55,10 +57,18 @@ const PomodoroTimer = ({ appSettings, setWorkDone, workDone }: Props) => {
 
         <div className='flex flex-col mt-10 items-center justify-center space-y-5'>
             <h1 className='text-xl font-semibold text-white'>{getStatusText(status)}</h1>
-            <Pie percentage={calculatePercentage(secondsRemaining, appSettings.timers[status] * 60)} handleStart={handleStart} handlePause={handlePause} timerStatus={timerStatus} />
-            <h1 className='text-3xl font-semibold text-white'>{getFormattedTime(secondsRemaining)}</h1>
-            <p onClick={handlePrevious}>Prev</p>
-            <p onClick={handleNext}>Next</p>
+            <div className='flex items-center max-w-lg w-full justify-between'>
+                <Image onClick={handlePrevious} src={PreviousIcon} objectFit="contain" className={`${timerStatus === TIMER_STATUS.PAUSED ? 'opacity-100' : 'opacity-0'} transition-all ease-in-out duration-200 invert cursor-pointer invisible xs:visible `} height={50} width={50} />
+                <Pie percentage={calculatePercentage(secondsRemaining, appSettings.timers[status] * 60)} handleStart={handleStart} handlePause={handlePause} timerStatus={timerStatus} />
+                <Image onClick={handleNext} src={NextIcon} objectFit="contain" className={`${timerStatus === TIMER_STATUS.PAUSED ? 'opacity-100' : 'opacity-0'} transition-all ease-in-out duration-200 invert cursor-pointer invisible xs:visible `} height={50} width={50} />
+
+            </div>
+            <div className='flex items-center justify-between'>
+                <Image onClick={handlePrevious} src={PreviousIcon} objectFit="contain" className={`${timerStatus === TIMER_STATUS.PAUSED ? 'opacity-100' : 'opacity-0'} transition-all ease-in-out duration-200 invert cursor-pointer visible xs:invisible`} height={30} width={30} />
+                <h1 className='text-3xl font-semibold text-white mx-5'>{getFormattedTime(secondsRemaining)}</h1>
+                <Image onClick={handleNext} src={NextIcon} objectFit="contain" className={`${timerStatus === TIMER_STATUS.PAUSED ? 'opacity-100' : 'opacity-0'} transition-all ease-in-out duration-200 invert cursor-pointer visible xs:invisible `} height={30} width={30} />
+
+            </div>
 
             <div onClick={handleReset} className={`${timerStatus === TIMER_STATUS.PAUSED && calculatePercentage(secondsRemaining, appSettings.timers[status] * 60) !== 100 ? 'opacity-100' : 'opacity-0'} hover:animate-spin cursor-pointer transition-all ease-in-out duration-500 flex  items-center justify-center absolute bottom-10`}>
                 <Image src={ResetIcon} objectFit="contain" className="invert" height={30} width={30} />
