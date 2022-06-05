9f69aad0b4ca0e23974ce6c0496f7d051f9c7bdd
JethroSama <kel73896@gmail.com>
2022-06-05 16:16:27 +0800

    style settings

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -30,8 +30,14 @@ const Settings = ({ settingsOpen, setSettingsOpen, appSettings, setAppSettings }
             <div className='flex justify-end w-full h-16 px-4'>
                 <Image onClick={handleClose} src={CloseIcon} objectFit="contain" className="invert cursor-pointer" height={30} width={30} />
             </div>
-            <div>
-                <TimeSetter title="Work time" timeType={STATUS.WORK} time={appSettings.timers[STATUS.WORK]} setTime={setTime} />
+            <div className="space-y-3">
+
+                <h1 className='text-3xl text-white font-semibold text-center '>Adjust Timer(Minutes)</h1>
+                <div className='flex space-x-3 justify-center items-center flex-wrap'>
+                    <TimeSetter title="Work" timeType={STATUS.WORK} time={appSettings.timers[STATUS.WORK]} setTime={setTime} />
+                    <TimeSetter title="Short Break" timeType={STATUS.BREAK} time={appSettings.timers[STATUS.BREAK]} setTime={setTime} />
+                    <TimeSetter title="Long Break" timeType={STATUS.LONG_BREAK} time={appSettings.timers[STATUS.LONG_BREAK]} setTime={setTime} />
+                </div>
             </div>
         </div>
     )
