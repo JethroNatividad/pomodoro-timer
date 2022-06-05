52ce33117c0630707caa35dbe076c821966937f9
JethroSama <kel73896@gmail.com>
2022-06-05 12:50:45 +0800

    Create next status to own function

--- lib/timer.ts
+++ lib/timer.ts
@@ -1,3 +1,6 @@
+import { Dispatch, SetStateAction } from "react";
+import { AppSettings, STATUS, TIMER_STATUS } from "../types";
+
 const getFormattedTime = (timestamp: number) => {
     const hours = Math.floor(timestamp / 60 / 60)
     const minutes = Math.floor(timestamp / 60) - (hours * 60)
@@ -7,6 +10,44 @@ const getFormattedTime = (timestamp: number) => {
 }
 export const calculatePercentage = (current: number, total: number) => {
     return Math.floor((current / total) * 100)
+}
+
+type NextStatusProps = {
+    status: STATUS
+    setWorkDone: Dispatch<SetStateAction<number>>
+    setStatus: Dispatch<SetStateAction<STATUS>>
+    setSecondsRemaining: Dispatch<SetStateAction<number>>
+    setTimerStatus: Dispatch<SetStateAction<TIMER_STATUS>>
+    appSettings: AppSettings
+    workDone: number
+}
+
+export const nextStatus = ({ status, setTimerStatus, setWorkDone, setStatus, setSecondsRemaining, appSettings, workDone }: NextStatusProps) => {
+    if (status === STATUS.WORK && workDone === 3) {
+        setTimerStatus(TIMER_STATUS.PAUSED)
+        setWorkDone(workDone + 1)
+        setStatus(STATUS.LONG_BREAK)
+        return setSecondsRemaining(appSettings.timers.longBreakTime * 60)
+    }
+
+    if (status === STATUS.WORK) {
+        setTimerStatus(TIMER_STATUS.PAUSED)
+        setWorkDone(workDone + 1)
+        setStatus(STATUS.BREAK)
+        return setSecondsRemaining(appSettings.timers.breakTime * 60)
+    }
+
+    if (status === STATUS.BREAK) {
+        setTimerStatus(TIMER_STATUS.PAUSED)
+        setStatus(STATUS.WORK)
+        return setSecondsRemaining(appSettings.timers.workTime * 60)
+    }
+
+    if (status === STATUS.LONG_BREAK) {
+        setTimerStatus(TIMER_STATUS.PAUSED)
+        setStatus(STATUS.WORK)
+        return setSecondsRemaining(appSettings.timers.workTime * 60)
+    }
 }
 
 export default getFormattedTime
