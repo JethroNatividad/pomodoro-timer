55347687ea75ff9e7c89d5841d3390e757a0c425
JethroSama <kel73896@gmail.com>
2022-06-05 14:12:47 +0800

    set settings props

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
+import { AppSettings } from '../types'
 
-type Props = {}
+type Props = {
+    appSettings: AppSettings
+    setAppSettings: Dispatch<SetStateAction<AppSettings>>
+    setSettingsOpen: Dispatch<SetStateAction<boolean>>
+    settingsOpen: boolean
+}
 
 const Settings = (props: Props) => {
     return (
