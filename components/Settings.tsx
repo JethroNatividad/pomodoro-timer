caa597bc797856e59952bcac7878b89ac90b6c8c
JethroSama <kel73896@gmail.com>
2022-06-05 14:43:07 +0800

    add and implement close icon

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,5 +1,7 @@
 import React, { Dispatch, SetStateAction } from 'react'
 import { AppSettings } from '../types'
+import CloseIcon from '../icons/close.svg'
+import Image from 'next/image'
 
 type Props = {
     appSettings: AppSettings
@@ -8,9 +10,16 @@ type Props = {
     settingsOpen: boolean
 }
 
-const Settings = (props: Props) => {
+const Settings = ({ settingsOpen, setSettingsOpen }: Props) => {
+    const handleClose = () => {
+        setSettingsOpen(false)
+    }
     return (
-        <div>Settings</div>
+        <div className={`${settingsOpen ? 'flex' : 'hidden'} transition-all delay-500 absolute right-0 top-0 max-w-xl w-full h-full bg-light-green shadow-lg`}>
+            <div className='flex justify-end w-full h-16 px-4'>
+                <Image onClick={handleClose} src={CloseIcon} objectFit="contain" className="invert cursor-pointer" height={30} width={30} />
+            </div>
+        </div>
     )
 }
 
