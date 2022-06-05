646691ece2678b7d483f1f2cc9196dcf631fa8d6
JethroSama <kel73896@gmail.com>
2022-06-05 12:51:06 +0800

    Refactor next status code, add prev and next

--- components/PomodoroTimer.tsx
+++ components/PomodoroTimer.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
-import getFormattedTime, { calculatePercentage } from '../lib/timer'
+import getFormattedTime, { calculatePercentage, nextStatus } from '../lib/timer'
 import { AppSettings, STATUS, TIMER_STATUS } from '../types'
 import Pie from './TimerCircle'
 import ResetIcon from '../icons/reset.svg'
@@ -33,32 +33,7 @@ const PomodoroTimer = ({ appSettings, setWorkDone, workDone }: Props) => {
                 setSecondsRemaining(secondsRemaining - 1)
 
                 if (secondsRemaining === 0) {
-                    if (status === STATUS.WORK && workDone === 3) {
-                        setTimerStatus(TIMER_STATUS.PAUSED)
-                        setWorkDone(workDone + 1)
-                        setStatus(STATUS.LONG_BREAK)
-                        return setSecondsRemaining(appSettings.timers.longBreakTime * 60)
-                    }
-
-                    if (status === STATUS.WORK) {
-                        setTimerStatus(TIMER_STATUS.PAUSED)
-                        setWorkDone(workDone + 1)
-                        setStatus(STATUS.BREAK)
-                        return setSecondsRemaining(appSettings.timers.breakTime * 60)
-                    }
-
-                    if (status === STATUS.BREAK) {
-                        setTimerStatus(TIMER_STATUS.PAUSED)
-                        setStatus(STATUS.WORK)
-                        return setSecondsRemaining(appSettings.timers.workTime * 60)
-                    }
-
-                    if (status === STATUS.LONG_BREAK) {
-                        setTimerStatus(TIMER_STATUS.PAUSED)
-                        setStatus(STATUS.WORK)
-                        return setSecondsRemaining(appSettings.timers.workTime * 60)
-                    }
-
+                    nextStatus({ status, setTimerStatus, setWorkDone, setStatus, setSecondsRemaining, appSettings, workDone })
                 }
             }, 1000)
             return () => clearInterval(interval)
@@ -80,12 +55,39 @@ const PomodoroTimer = ({ appSettings, setWorkDone, workDone }: Props) => {
         setSecondsRemaining(appSettings.timers[status] * 60)
     }
 
+    const handleNext = () => {
+        nextStatus({ status, setTimerStatus, setWorkDone, setStatus, setSecondsRemaining, appSettings, workDone })
+    }
+
+    const handlePrevious = () => {
+        if (status === STATUS.WORK) {
+            setTimerStatus(TIMER_STATUS.PAUSED)
+            setStatus(STATUS.BREAK)
+            return setSecondsRemaining(appSettings.timers.breakTime * 60)
+        }
+
+        if (status === STATUS.BREAK) {
+            setTimerStatus(TIMER_STATUS.PAUSED)
+            setStatus(STATUS.WORK)
+            return setSecondsRemaining(appSettings.timers.workTime * 60)
+        }
+
+        if (status === STATUS.LONG_BREAK) {
+            setTimerStatus(TIMER_STATUS.PAUSED)
+            setStatus(STATUS.WORK)
+            return setSecondsRemaining(appSettings.timers.workTime * 60)
+        }
+    }
+
     return (
 
         <div className='flex flex-col mt-10 items-center justify-center space-y-5'>
             <h1 className='text-xl font-semibold text-white'>{getStatusText(status)}</h1>
             <Pie percentage={calculatePercentage(secondsRemaining, appSettings.timers[status] * 60)} handleStart={handleStart} handlePause={handlePause} timerStatus={timerStatus} />
             <h1 className='text-3xl font-semibold text-white'>{getFormattedTime(secondsRemaining)}</h1>
+            <p onClick={handlePrevious}>Prev</p>
+            <p onClick={handleNext}>Next</p>
+
             <div onClick={handleReset} className={`${timerStatus === TIMER_STATUS.PAUSED && calculatePercentage(secondsRemaining, appSettings.timers[status] * 60) !== 100 ? 'opacity-100' : 'opacity-0'} hover:animate-spin cursor-pointer transition-all ease-in-out duration-500 flex  items-center justify-center absolute bottom-10`}>
                 <Image src={ResetIcon} objectFit="contain" className="invert" height={30} width={30} />
             </div>
